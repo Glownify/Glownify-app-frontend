55bb9a2b5d4504a8467d4a5b397c0019c2e6b842
Raman Chauhan <ramanchauhan13@gmail.com>
2025-10-31 12:08:01 +0530

    feat: Add registration screens for Salon Admin and Independent Professional, and implement admin type selection

--- src/navigation/AuthNavigator.jsx
+++ src/navigation/AuthNavigator.jsx
@@ -5,6 +5,9 @@ import RegisterScreen from '../screens/Auth/RegisterScreen';
 import ForgotPasswordScreen from '../screens/Auth/ForgotPasswordScreen';
 import EmailVerificationScreen from '../screens/Auth/EmailVerificationScreen';
 import ResetPasswordScreen from '../screens/Auth/ResetPasswordScreen';
+import SelectAdminTypeScreen from '../screens/Auth/SelectAdminTypeScreen';
+import SalonAdminRegisterScreen from '../screens/Auth/SalonAdminRegisterScreen';
+import IndependentProRegisterScreen from '../screens/Auth/IndependentProRegisterScreen';
 
 const Stack = createNativeStackNavigator();
 
@@ -16,6 +19,9 @@ export default function AuthNavigator() {
       <Stack.Screen name="ForgotPassword" component={ForgotPasswordScreen} />
       <Stack.Screen name="EmailVerification" component={EmailVerificationScreen} />
       <Stack.Screen name="ResetPassword" component={ResetPasswordScreen} />
+      <Stack.Screen name="SelectAdminType" component={SelectAdminTypeScreen} />
+      <Stack.Screen name="SalonAdminRegister" component={SalonAdminRegisterScreen} />
+      <Stack.Screen name="IndependentProRegister" component={IndependentProRegisterScreen} />
     </Stack.Navigator>
   );
 }
