cb36fe7da7a6afc20a06562bbe3a35820788e8d7
Raman Chauhan <ramanchauhan13@gmail.com>
2025-12-26 00:03:55 +0530

    fix: Update API base URL to use the correct production endpoint

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 // export const API_BASE_URL = 'https://saloonbackend-mumt.onrender.com/api';
-
-export const API_BASE_URL = 'http://192.168.29.20:3000/api';
+export const API_BASE_URL='https://prozac-documentation-pioneer-groove.trycloudflare.com/api';
+// export const API_BASE_URL = 'http://192.168.29.20:3000/api';
 const axiosInstance = axios.create({
     baseURL: API_BASE_URL,
     headers: {
