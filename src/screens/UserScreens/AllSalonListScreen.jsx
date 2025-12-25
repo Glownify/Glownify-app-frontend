f568c331cf283370685da0046841c471c09a611d
Moksh-Git <patilmokshadg@gmail.com>
2025-12-26 01:27:33 +0530

    populating individual salon data

--- src/screens/UserScreens/AllSalonListScreen.jsx
+++ src/screens/UserScreens/AllSalonListScreen.jsx
@@ -12,12 +12,13 @@ import Icon from 'react-native-vector-icons/Ionicons';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchAllSalonsByCategory } from '../../redux/slices/userSlice';
 import { useEffect } from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 const salonData = [
   {
     id: 1,
     name: 'Glow Beauty Studio',
-    image: require("../../assets/salonInterior.jpg"),
+    image: require('../../assets/salonInterior.jpg'),
     // image: 'https://via.placeholder.com/350x180',
     services: 'Haircut ₹299 · Hair Color ₹899 · Facial ₹499',
     distance: '1.2 km',
@@ -67,7 +68,7 @@ const salonData = [
 
 export default function AllSalonListScreen({ navigation, route }) {
   const dispatch = useDispatch();
-  const {allSalons, loading, error} = useSelector(state => state.user);
+  const { allSalons, loading, error } = useSelector(state => state.user);
   const { category, lat, lng } = route.params;
 
   console.log('AllSalonListScreen salons from redux:', allSalons);
@@ -79,51 +80,70 @@ export default function AllSalonListScreen({ navigation, route }) {
   }, [category, lat, lng, dispatch]);
 
   return (
-    <View style={styles.container}>
-      {/* --- Header --- */}
-      <View style={styles.header}>
-        <Text style={styles.headerTitle}>Popular Near Me</Text>
-        {/* This button lets you close the modal */}
-        <TouchableOpacity onPress={() => navigation.goBack()}>
-          <Icon name="chevron-down-outline" size={28} color="#333" />
-        </TouchableOpacity>
-      </View>
-
-      {/* Salon List */}
-      <ScrollView showsVerticalScrollIndicator={false}>
-        {salonData.map(salon => (
-          <TouchableOpacity key={salon.id} style={styles.card}>
-            <Image source={require("../../assets/salonInterior.jpg")} style={styles.image} />
-
-            <View style={styles.cardContent}>
-              <Text style={styles.salonName}>{salon.name}</Text>
-              <Text style={styles.services}>{salon.services}</Text>
-
-              
-
-              <View style={styles.footerRow}>
-                <Text style={styles.footerText}>📍 {salon.distance}</Text>
-                <Text style={styles.footerText}>⭐ {salon.rating}</Text>
-              </View>
-            </View>
+    <SafeAreaView style={styles.safeArea} edges={['top']}>
+      <View style={styles.container}>
+        {/* --- Header --- */}
+        <View style={styles.header}>
+          <Text style={styles.headerTitle}>Popular Near Me</Text>
+          {/* This button lets you close the modal */}
+          <TouchableOpacity onPress={() => navigation.goBack()}>
+            <Icon name="chevron-down-outline" size={28} color="#333" />
           </TouchableOpacity>
-        ))}
-      </ScrollView>
-    </View>
+        </View>
+
+        {/* Salon List */}
+        <ScrollView
+          contentContainerStyle={{ paddingBottom: 65 }}
+          showsVerticalScrollIndicator={false}
+        >
+          {allSalons.map(salon => (
+            <TouchableOpacity
+              key={salon._id}
+              style={styles.card}
+              onPress={() =>
+                navigation.navigate('ShopDetailsFull', { salonId: salon._id })
+              }
+            >
+              <Image
+                source={require('../../assets/salonInterior.jpg')}
+                style={styles.image}
+              />
+
+              <View style={styles.cardContent}>
+                <Text style={styles.salonName}>{salon.shopName}</Text>
+                <Text style={styles.services}>
+                  Bridal Makeup ₹5,999 · Threading ₹99
+                </Text>
+
+                <View style={styles.footerRow}>
+                  <Text style={styles.footerText}>
+                    📍 {(salon.distanceInMeters / 1000).toFixed(2)}
+                  </Text>
+                  <Text style={styles.footerText}>⭐ 4.8</Text>
+                </View>
+              </View>
+            </TouchableOpacity>
+          ))}
+        </ScrollView>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#156778',
+  },
   header: {
     flexDirection: 'row',
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingVertical: 15,
-    marginVertical: 15,
-    backgroundColor: '#FFFFFF', // White header background
+    marginBottom: 10,
+    marginTop: -10,
     marginHorizontal: -20, // Extend to screen edges
     paddingHorizontal: 20, // Re-apply padding
-    
   },
   headerTitle: {
     fontSize: 20,
@@ -132,7 +152,6 @@ const styles = StyleSheet.create({
   },
 
   container: {
-    flex: 1,
     backgroundColor: '#F8F8F8',
     padding: 16,
   },
@@ -196,14 +215,14 @@ const styles = StyleSheet.create({
   salonName: {
     fontSize: 22,
     fontWeight: '700',
-    marginBottom: 10
+    marginBottom: 10,
   },
 
   services: {
     fontSize: 13,
     color: '#666',
     marginVertical: 4,
-    marginBottom: 10
+    marginBottom: 10,
   },
 
   footerRow: {
