e592e4abb5319aaed91d10db819913c0e4e13d92
Prashant Shaw <prashant.shaw926@gmail.com>
2026-02-11 10:29:33 +0530

    Integrate NativeWind and Tailwind CSS for styling by adding configuration, dependencies, and type definitions.

--- metro.config.js
+++ metro.config.js
@@ -1,17 +1,5 @@
-// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
-// /**
-//  * Metro configuration
-//  * https://reactnative.dev/docs/metro
-//  *
-//  * @type {import('@react-native/metro-config').MetroConfig}
-//  */
-// const config = {};
-
-// module.exports = mergeConfig(getDefaultConfig(__dirname), config);
-
-
 const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
+const { withNativeWind } = require('nativewind/metro');
 
 const defaultConfig = getDefaultConfig(__dirname);
 
@@ -27,24 +15,6 @@ const config = {
   },
 };
 
-module.exports = mergeConfig(defaultConfig, config);
+const mergedConfig = mergeConfig(defaultConfig, config);
 
-
-
-// const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config');
-
-// const defaultConfig = getDefaultConfig(__dirname);
-
-// const config = {
-//   transformer: {
-//     babelTransformerPath: require.resolve('react-native-svg-transformer'),
-//   },
-//   resolver: {
-//     // Remove svg from assetExts
-//     assetExts: defaultConfig.resolver.assetExts.filter(ext => ext !== 'svg'),
-//     // Add svg to sourceExts
-//     sourceExts: [...defaultConfig.resolver.sourceExts, 'svg'],
-//   },
-// };
-
-// module.exports = mergeConfig(defaultConfig, config);
+module.exports = withNativeWind(mergedConfig, { input: './global.css' });
